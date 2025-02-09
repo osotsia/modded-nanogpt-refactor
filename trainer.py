57b2b815d6a804d533a36a0864b5e9005983bf9c
osotsia <osotsi.a@gmail.com>
2025-02-09 17:34:37 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -114,8 +114,8 @@ class Hyperparameters:
     rank: int = rank
     # PEER
     peer_n_experts = 512 * 512
-    peer_query_dim = 128
-    peer_topk = 16
+    peer_query_dim = 256
+    peer_topk = 32
     peer_n_heads = 4
     peer_activation = F.gelu
     peer_query_batchnorm = True
